94e0e9165b8554cdb0c3aab2502df11c368ccf18
Mario181003 <mariogutirosales123@gmail.com>
2024-06-25 10:28:20 -0600

    Actualizacion back

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -13,68 +13,33 @@ class model_user(BaseModel):
     created_at: datetime = datetime.now()
     estatus:bool=False
 
-class model_persona(BaseModel):
-    id: str
-    nombre: str
-    apellido_pat: str
-    apellido_mat: str
-    direccion: str
-    telefono: str
-    created_at: datetime = datetime.now()
-    estatus:bool=False
-
 @user.get("/")
 
 def bienvenido():
     return "Bienvenido al sistema de API's"
 
-@user.get("/users")
+@user.get('/users', tags=["Usuarios"])
 
 def get_usuarios():
     return users
 
-@user.post('/users')
+@user.post('/users', tags=["Usuarios"])
 def save_usuarios(insert_users:model_user):
     users.append(insert_users)
     #print(insert_users)
     return "Datos guardados"
 
-@user.put('/users/{user_id}')
+@user.put('/users/{user_id}', tags=["Usuarios"])
 def update_usuario(user_id: str, updated_user:model_user):
     for index, user in enumerate(users):
         if user.id == user_id:
             users[index] = updated_user
             return {"message": "Datos actualizados"}
         
-@user.delete('/users/{user_id}')
+@user.delete('/users/{user_id}', tags=["Usuarios"])
 def delete_usuario(user_id: str):
     for index, user in enumerate(users):
         if user.id == user_id:
             del users[index]
             return {"message": "Usuario eliminado"}
 
-@persona.get('personas')
-@persona.get("/personas")
-
-def get_personas():
-    return personas
-
-@persona.post('/personas')
-def save_personas(insert_personas:model_persona):
-    personas.append(insert_personas)
-    #print(insert_users)
-    return "Datos guardados"
-
-@persona.put('/personas/{persona_id}')
-def update_persona(persona_id: str, updated_persona:model_persona):
-    for index, persona in enumerate(personas):
-        if persona.id == persona_id:
-            personas[index] = updated_persona
-            return {"message": "Datos actualizados"}
-        
-@persona.delete('/personas/{persona_id}')
-def delete_persona(persona_id: str):
-    for index, persona in enumerate(personas):
-        if persona.id == persona_id:
-            del personas[index]
-            return {"message": "Persona eliminado"}
