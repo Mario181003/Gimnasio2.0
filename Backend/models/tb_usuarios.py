8594f45e436cd0712c5c230c0fd85e1d2accf15c
Mario181003 <mariogutirosales123@gmail.com>
2024-06-27 10:53:56 -0600

    Creacion de modelos

---
+++ Backend/models/tb_usuarios.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from config.db import Base
+
+class usuarios(Base):
+    __tablename__ = 'usuarios'
+
+    id = Column(Integer, primary_key=True, index=True),
+    usuario = Column(String(255), index=True),
+    password = Column(String(255), index=True),
+    estatus = Column(Boolean, index=True)
