cf53cc369b6706c560dcdef3056abd177a2e0742
Mario181003 <mariogutirosales123@gmail.com>
2024-07-02 10:50:52 -0600

    Avance modelo usuarios

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from routes.user import user
-from routes.persona import persona
-app=FastAPI()
+# from routes.person import person
+
+app=FastAPI(
+    title="Gimnasio S.A. de C.V.",
+    description="API para el almacenamiento de informacipn de un gimnasio"
+)
 app.include_router(user)
-app.include_router(persona)
+# app.include_router(person)
 
 print ("Hola bienvenido a mi backend")
--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -18,35 +18,35 @@ def get_db():
     finally:
         db.close()
 
-@user.get("/users", response_model=list[schemas.users.User],tags=["Usuarios"] )
-def read_users(skip: int=0, limit: int=10, db: Session=Depends(get_db)):
-    db_users = cruds.users.get_users(db=db, skip=skip, limit=limit)
+@user.get("/users/", response_model=List[schemas.users.User], tags=["Usuarios"])
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    db_users= cruds.users.get_users(db=db, skip=skip, limit=limit)
     return db_users
 
-@user.post("/users/{id}", response_model=schemas.users.User, tags=["Usuarios"] )
-def read_user(skip: int=0, db: Session=Depends(get_db)):
-    db_user = cruds.users.get_user(db=db, id=id)
+@user.post("/user/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+def read_user(id: int, db: Session = Depends(get_db)):
+    db_user= cruds.users.get_user(db=db, id=id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-@user.post("/users/", response_model=schemas.users.User, tags=["Usuarios"] )
-def create_user(user: schemas.users.UserCreate, db: Session=Depends(get_db)):
+@user.post("/users/", response_model=schemas.users.User, tags=["Usuarios"])
+def create_user(user: schemas.users.UserCreate, db: Session = Depends(get_db)):
     db_user = cruds.users.get_user_by_usuario(db, usuario=user.usuario)
     if db_user:
         raise HTTPException(status_code=400, detail="Usuario existente intenta nuevamente")
     return cruds.users.create_user(db=db, user=user)
 
-@user.put("/users/{user_id}", response_model=schemas.users.User, tags=["Usuarios"])
-def update_user(id: int, user: schemas.users.UserUpdate, db: Session=Depends(get_db)):
+@user.put("/user/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+def update_user(id: int, user: schemas.users.UserUpdate, db: Session = Depends(get_db)):
     db_user = cruds.users.update_user(db=db, id=id, user=user)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Usuario no existe, no actualizado")
     return db_user
 
-@user.delete("/users/{user_id}", response_model=schemas.users.User, tags=["Usuarios"])
-def delete_user(id: int, db: Session=Depends(get_db)):
+@user.delete("/user/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+def delete_user(id: int, db: Session = Depends(get_db)):
     db_user = cruds.users.delete_user(db=db, id=id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="Usuario no existe, no se puede eliminar")
+        raise HTTPException(status_code=404, detail="Usuario no existe, no se pudo eliminar")
     return db_user
