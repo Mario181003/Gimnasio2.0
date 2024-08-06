28e3ef6d7fd51c3f53f9a5ff019c8aef9d9e4ede
Mario181003 <mariogutirosales123@gmail.com>
2024-08-06 10:19:56 -0600

    Modelos de programas saludables

--- Backend/app.py
+++ Backend/app.py
@@ -5,6 +5,7 @@ from routes.roles import rol
 from routes.usuarios_roles import userrol
 from routes.rutinas import rutina
 from routes.ejercicios_rutinas import ejcrtn
+from routes.progra_salud import progra_salud
 
 app=FastAPI(
     title="Bull´s GYM",
@@ -16,5 +17,6 @@ app.include_router(rol)
 app.include_router(userrol)
 app.include_router(rutina)
 app.include_router(ejcrtn)
+app.include_router(progra_salud)
 
 print ("Hola bienvenido a mi backend")
